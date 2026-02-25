63754bd5b989550fbd71250f7b78061aa52554da
Saif Uddin Bijoy <Bijoy@Fs-Mac-mini.local>
2026-02-25 15:55:18 +0600

    Intialize everything

--- woocommerce-filter-with-ajax.php
+++ woocommerce-filter-with-ajax.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WooCommerce Filter with Ajax with Elementor Integration
  * Description: A plugin to filter WooCommerce products using Ajax. Allows customers to filter products by category, price, attributes, and more with real-time Ajax updates and seamless Elementor widget integration.
- * Version: 1.0
+ * Version: 1.0.0
  * Author: Forazitech
  * Author URI: https://forazitech.com
  * License: GPL2
@@ -10,3 +10,52 @@
  * Requires plugins: woocommerce, elementor
  */
 
+defined("ABSPATH") or die("Direct access not allowed.");
+
+class Wc_filter_ajax_with_elementor{
+    private static $instance = null;
+    public static function get_instance(){
+        if (self::$instance == null) self::$instance = new self();
+        return self::$instance;
+    }
+
+    public function __construct(){
+        add_action("plugins_loaded", [$this, "init"]);
+    }
+
+    public function init(){
+        $this->define_constants();
+        $this->includes_files();
+        
+        add_action("wp_enqueue_scripts", [$this, "enqueue_scripts"]);
+        
+        add_shortcode("wc_filter_ajax", [$this, "render_shortcode"]);
+        
+        register_activation_hook(__FILE__, [$this, "activate"]);
+        register_deactivation_hook(__FILE__, [$this, "deactivate"]);
+    }
+
+    private function define_constants(){
+        define("WC_FILTER_AJAX_VERSION", "1.0.0");
+        define("WC_FILTER_AJAX_PLUGIN_DIR", plugin_dir_path(__FILE__));
+        define("WC_FILTER_AJAX_PLUGIN_URL", plugin_dir_url(__FILE__));
+    }
+
+    private function includes_files(){
+        
+    }
+
+    public function enqueue_scripts(){
+        // Enqueue necessary scripts and styles here
+    }
+
+    public function activate(){
+        // Activation code here
+    }
+
+    public function deactivate(){
+        // Deactivation code here
+    }
+}
+
+Wc_filter_ajax_with_elementor::get_instance();
