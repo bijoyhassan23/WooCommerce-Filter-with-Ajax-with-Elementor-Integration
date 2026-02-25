686e871ffdadb0efae0953490b21f90a1b83984d
Saif Uddin Bijoy <Bijoy@Fs-Mac-mini.local>
2026-02-25 14:52:37 +0600

    Inintialize

---
+++ woocommerce-filter-with-ajax.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Filter with Ajax with Elementor Integration
+ * Description: A plugin to filter WooCommerce products using Ajax. Allows customers to filter products by category, price, attributes, and more with real-time Ajax updates and seamless Elementor widget integration.
+ * Version: 1.0
+ * Author: Forazitech
+ * Author URI: https://forazitech.com
+ * License: GPL2
+ * Text Domain: woocommerce-filter-with-ajax
+ * Requires plugins: woocommerce, elementor
+ */
+
